068c057535a48ed63f3a0a98aae507fe37bd1732
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-09-20 23:27:15 -0600

    Text rendered - first component

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,15 @@
 
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import {
+    Text,
+    AppRegistry
+} from 'react-native';
 
 // Create a component
-const App = () => {
-    return (
+const App = () => (
         <Text>Some Text</Text>
     );
-};
 
 // Render it to the device
+AppRegistry.registerComponent('albums', () => App);
